0499bfb477f0048590d9fa081bb475005fe4cefd
TitusCharlie <tituslifecharlie@gmail.com>
2023-02-21 00:49:50 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,6 @@ int main(void)
 	printf("size of an int: %zu byte(s)\n, sizeof(int)");
 	printf("size of a long int: %zu byte(s)\n, sizeof(long int)");
 	printf("size of a long long int: %zu byte(s)\n, sizeof(long long int)");
-	printf("size of char: %zu byte(s)\n, sizeof(char)")
+	printf("size of float: %zu byte(s)\n, sizeof(float)");
+	return (0);
 }
