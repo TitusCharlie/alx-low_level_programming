c4eac6984f8506be6094637cc9f5fcb2c5a312c5
TitusCharlie <tituslifecharlie@gmail.com>
2023-02-23 13:05:55 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,12 @@
  */
 
 void print_numbers(void)
+
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-return (c);
-_putchar(c);
-_putchar('\n');
+	char c;
+	for (c = '0'; c < '9';  c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
