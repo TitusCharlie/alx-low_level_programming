92ae7ad1abb8c725c31a206e1216c30956143afb
TitusCharlie <tituslifecharlie@gmail.com>
2023-03-13 05:33:09 -0700

    add @ to argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  * main - print the name of the program
  * @argc: count arguments
- * argv: Arguments
+ * @argv: Arguments
  * Return: Always 0 (Success)
  */
 
@@ -12,5 +12,6 @@ int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
 	printf("%i\n", argc - 1);
+
 	return (0);
 }
