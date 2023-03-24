6f65f3eefb410cddb3c2e4207c37cfb35c9bb4bb
TitusCharlie <tituslifecharlie@gmail.com>
2023-03-24 10:14:02 -0700

    done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,40 +10,42 @@
 
 void print_all(const char * const format, ...)
 {
-        va_list valist;
-        unsigned int i = 0, j, c = 0;
-        char *str;
-        const char t_arg[] = "cifs";
+	int i = 0;
+	char *str, *sep = "";
 
-        va_start(valist, format);
-        while (format && format[i])
-        {
-                if (format[i] == t_arg[j] && c)
-                {
-                        printf(", ");
-                        break;
-                }
-                j++;
-        }
-        switch (format[i])
-        {
-                case 'c':
-                        printf("%c", va_arg(valist, int)), c = 1;
-                        break;
-                case 'i':
-                        printf("%d", va_arg(valist, int)), c = 1;
-                        break;
-                case 'f':
-                        printf("%f", va_arg(valist, double)), c = 1;
-                        break;
-                case 's':
-                        str = va_arg(valist, char *), c = 1;
-                        if (!str)
-                        {
-                                printf("(nil)");
-                                break;
+	va_list list; 
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep,  va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char*);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
                         }
-                        i++;
-        }
-        printf("\n"), va_end(valist);
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(list);
 }
