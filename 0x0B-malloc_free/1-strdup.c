d19afcb2ff0282c748d4e073d4e334d1b722c5cf
TitusCharlie <tituslifecharlie@gmail.com>
2023-03-24 16:54:26 -0700

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,66 +1,30 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- *_strlen - count array
- *@s: array of elements
- *Return: 1
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
  */
-
-int _strlen(char *s)
+char *_strdup(char *str)
 {
-	unsigned int i;
+	char *aaa;
+	int i, r = 0;
 
+	if (str == NULL)
+		return (NULL);
 	i = 0;
-	while (s[i] != '\0')/*count character of string*/
-	{
+	while (str[i] != '\0')
 		i++;
-	}
-	return (i);
-}
-/**
- *_strcpy - copy arrays
- *@src: array of elements
- *@dest: dest array
- *Return: dest
- */
 
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
+	aaa = malloc(sizeof(char) * (i + 1));
 
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-/**
- *_strdup - array for prints a string
- *@str: array of elements
- *@strcpy: copy the array
- *Return: poiter
- */
-
-char *strdup(char *str)
-{
-	char *dst;
-	unsigned int size;
-
-	if (str == 0)
-	{
+	if (aaa == NULL)
 		return (NULL);
-	}
-	size = _strlen(str) + 1;
-	dst = (char *) malloc(size * sizeof(char));
 
-	if (dst == 0)
-	{
-		return (NULL);
-	}
-	_strcpy(dst, str);
-	return (dst);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+
+	return (aaa);
 }
+
