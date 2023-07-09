2f43af2c09f5b13cd52083590441e7d92b0d9e51
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-09 20:42:13 +0100

    done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,7 +6,7 @@
  * @key: key you are looking for
  *
  * Return:  value associated with the element,
- * 	    or NULL if key couldn’t be found
+ *          or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
